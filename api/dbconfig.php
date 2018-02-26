131e470012b390ea998de31381ed352d6b50db73
x11 <praxisbit1934@gmail.com>
2018-02-27 01:57:10 +0530

    Final Commit

--- api/dbconfig.php
+++ api/dbconfig.php
@@ -1,8 +1,7 @@
 <!-- This is a temporary database -->
-
 <?php
 date_default_timezone_set('Asia/Kolkata');
-$link = mysqli_connect("localhost", "root", "", "farma");
+$link = mysqli_connect("localhost", "root", "", "userdatabase");
 if($link === false)
 {
     die("ERROR: Could not connect. " . mysqli_connect_error());
